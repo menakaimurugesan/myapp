54815854a19992b43be03081b9785e4cba004ca1
Menakai <menakai@tinymagiq.com>
2017-06-08 13:38:43 +0530

    Modify welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Tiny Magiq
                 </div>
 
                 <div class="links">
